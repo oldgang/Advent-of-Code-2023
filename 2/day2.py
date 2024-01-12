68ee8a04134e2e6b832926ee65a3026bbf847b7f
oldgang <iceman16999@gmail.com>
2024-01-12 12:17:30 +0100

    Day 2, part 2 finished

--- 2/day2.py
+++ 2/day2.py
@@ -18,7 +18,26 @@ def game_possible(game):
                 return False
     return True
 
-# # Read the example file
+# Find the minimum number of cubes needed to play the game (of each color)
+def find_minimum_cubes(game):
+    cubesNeeded = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
+    game = game.split(':')[1].strip()
+    rounds = game.split(';')
+    rounds = [round.strip() for round in rounds]
+    for round in rounds:
+        colors = round.split(',')
+        colors = [color.strip() for color in colors]
+        for color in colors:
+            cubeCount, color = color.split(' ')
+            if cubesNeeded[color] < int(cubeCount) or cubesNeeded[color] == 0:
+                cubesNeeded[color] = int(cubeCount)
+    return cubesNeeded
+
+# Read the example file
 # with open('2/example.txt', 'r') as f:
 #     lines = f.readlines()
 #     lines = [line.strip() for line in lines]
@@ -35,3 +54,10 @@ for line in lines:
         gameID = line.split(':')[0].split(' ')[1]
         sum += int(gameID)
 print('Part 1:', sum)
+
+# Part 2 - Find the minimum number of cubes needed to play the game and get a sum of their powers
+sum = 0
+for line in lines:
+    cubesNeeded = find_minimum_cubes(line)
+    sum += cubesNeeded['red'] * cubesNeeded['green'] * cubesNeeded['blue']
+print('Part 2:', sum)
